da055010ecf267716f92512c88bdeb8a31d7c3e5
AleksanderKhatkov9 <alekskhatkov91@gmail.com>
2020-11-19 15:26:40 +0300

    billed project on github commit2 2master

--- src/main/java/org/example/MusicPlayer.java
+++ src/main/java/org/example/MusicPlayer.java
@@ -1,14 +1,42 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
     private Music music;
+    private String name;
+    private int volume;
+    private List<Music> musicList = new ArrayList<Music>();
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
+    public MusicPlayer() {
+    }
 
-    public void playMusic(){
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public void playMusic() {
         System.out.println("Playing : " + music.getSong());
         System.out.println("Playing with autor: " + music.getAutor());
     }
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -18,6 +18,9 @@ public class TestSpring {
 //        Music music = context.getBean("musicBean",Music.class);
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
         musicPlayer.playMusic();
         context.close();
     }
